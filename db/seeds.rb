d984a3f329b58f9aeaa6474267a2cb71cde33abc
Yusuke <yusuke_10_19@yahoo.co.jp>
2022-06-26 01:26:40 +0900

    aaa

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,8 +21,8 @@ User.create!(
 end
 
 User.all.each do |user|
-  20.times {
+  20.times do
     content = Faker::Lorem.sentence(1)
     user.tasks.create!(content: content, done: 0, user_id: user.id)
-  }
+  end
 end
