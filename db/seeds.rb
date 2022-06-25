b0a483a0dce3b922b94b87d4e421bfe5eed0c3b1
Yusuke <yusuke_10_19@yahoo.co.jp>
2022-06-26 00:12:27 +0900

    seeds.rb  modified

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,21 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user = User.find(1)
-50.times do
+User.create!(
+  name: 'aaa'
+  email: aaa@aaa.com
+  passsword: aaaa
+)
+
+5.times do |n|
+  User.create!(
+    name: "テスト#{n+1}@"
+    email: "test@test.com"
+    passsword: "test"
+  )
+end
+
+User.all.each do |user|
   content = Faker::Lorem.sentence(1)
   user.tasks.create!(content: content, done: 0)
 end
