c94a341b7912514193959c4d965c3c3bc4b1049f
Yusuke <yusuke_10_19@yahoo.co.jp>
2022-06-26 01:29:28 +0900

    aaa

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(
-  name: 'aaa'
-  email: 'aaa@aaa.com'
-  passsword: 'aaaa'
-)
+User.create!( name: 'aaa', email: 'aaa@aaa.com', passsword: 'aaaa' )
 
 5.times do |n|
-  User.create!(
-    name: "テスト#{n+1}@"
-    email: "test#{n+1}@test.com"
-    passsword: "test"
-  )
+  User.create!( name: "テスト#{n+1}@", email: "test#{n+1}@test.com", passsword: "test" )
 end
 
 User.all.each do |user|
